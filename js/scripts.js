1e0dfeee5b745ab4357b2de0c7939a15ccd9eb32
Jake Kaad and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-02-09 14:44:07 -0800

    Add HTML & jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,24 @@ var isDiagonalMovement = function(start, finish) {
 var equalArray = function(arr1, arr2) {
   return (arr1[0] === arr2[0] && arr1[1] === arr2[1]);
 };
+
+
+
+$(document).ready(function() {
+  $("form#queen-attack").submit(function(event){
+    var x1 = parseInt($("input#x1").val());
+    var y1 = parseInt($("input#y1").val());
+    var x2 = parseInt($("input#x2").val());
+    var y2 = parseInt($("input#y2").val());
+    var result = queenAttack([x1, y1], [x2, y2]);
+
+    if (result) {
+      $(".not").text("");
+      $("#result").show();
+    } else {
+      $(".not").text(" not");
+      $("#result").show();
+    };
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -19,3 +19,19 @@ describe('queenAttack', function() {
     expect(queenAttack([2,1], [1,2])).to.equal(true);
   });
 });
+
+describe("isDiagonalMovement", function() {
+  it('is false if the movement is not diagonal', function(){
+    expect(isDiagonalMovement([1, 1], [1, 2])).to.equal(false);
+  });
+  
+  it('is true if the movement is diagonal', function(){
+    expect(isDiagonalMovement([1, 1], [2, 2])).to.equal(true);
+  });
+});
+
+describe("equalArray", function() {
+  it('is false when two arrays are not equal', function() {
+    expect(equalArray([1,1], [3,2])).to.equal(false);
+  });
+});
